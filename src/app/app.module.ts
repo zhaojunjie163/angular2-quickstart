574cade91ab59e44763ff108d157537d6d007214
Guavaer <zhaojunjie.mike@gmail.com>
2017-02-25 09:43:36 +0800

    this is to illustrate two way binding

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,7 +11,7 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    FormsModule,
+    FormsModule,// to leverage ngModel built-in directive, we have to import this module.
     HttpModule
   ],
   providers: [],
